b054c703b83f9bf117d0cfc4945e6ca3ccf9fca3
sidhyaashu <ashutoshsidhya69@gmail.com>
2024-08-24 12:38:04 +0530

    yet to work

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Chat Bot',
+    version= '0.0.1',
+    author= 'Asutosh sidhya',
+    author_email= 'ashutoshsidhya69@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
